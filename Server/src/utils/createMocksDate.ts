6fef295ae30ef5c1e5d5e5aafa5f928979c827d2
Juan Cruz Ledesma <juancruzledesma@yahoo.com.ar>
2023-08-02 18:24:21 -0300

    .

--- Server/src/utils/createMocksDate.ts
+++ Server/src/utils/createMocksDate.ts
@@ -16,12 +16,12 @@ const createMocksDateHandler = async () => {
 export { createMocksDateHandler }
 
 const createUserAdmin = async () => {
-    const userAdmin = await collections.Users.findOne({
+    const userAdmin = await collections.User.findOne({
         email: config.get("email_admin")
     })
     
     if (userAdmin === null) {
-        await collections.Users.create({
+        await collections.User.create({
             email: config.get("email_admin"),
             password: await bcrypt.encrypt(config.get("password_admin")),
         })
@@ -29,10 +29,10 @@ const createUserAdmin = async () => {
 }
 
 const createCategories = async () => {
-    const categoriesList = await collections.Categories.find({})
+    const categoriesList = await collections.Category.find({})
 
     if (categoriesList.length === 0) {
-        await collections.Categories.insertMany([
+        await collections.Category.insertMany([
             { name: 'Front-End:' }, { name: 'Back-End:' },
             { name: 'Otros:' }, { name: 'Proximamente:' },
         ])
