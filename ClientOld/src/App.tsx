1ccdd10ad22595c012ad04996b921387c8f976f5
Juan Cruz Ledesma <juancruzledesma@yahoo.com.ar>
2023-09-11 14:16:28 -0300

    new Client with Vite

---
+++ ClientOld/src/App.tsx
@@ -0,0 +1,34 @@
+import { Fragment } from "react";
+
+import "./App.css";
+import { Header } from "./Components/Header/Header";
+import { Home } from "./Components/Home/Home";
+import { AboutMe } from "./Components/AboutMe/AboutMe";
+import { DoneProjects } from "./Components/DoneProjects/DoneProjects";
+import { Footer } from "./Components/Footer/Footer";
+import { Contact } from "./Components/Contact/Contact";
+
+// localStorage.setItem("leng", "ES")
+
+function App() {
+
+  return (
+    <Fragment>
+      <Header />
+
+      <main>
+        <Home />
+
+        <AboutMe />
+
+        <DoneProjects />
+
+        <Contact />
+
+        <Footer />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
