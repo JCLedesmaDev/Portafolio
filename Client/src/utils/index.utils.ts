7bdfc1c80d71bf7520b53613498659d80d4dc0af
Juan Cruz Ledesma <juancruzledesma@yahoo.com.ar>
2023-09-13 17:32:40 -0300

    Client in progress

--- Client/src/utils/index.utils.ts
+++ Client/src/utils/index.utils.ts
@@ -1,3 +1,3 @@
 export { apiSrv } from './apiSrv'
 export type { IConfigInit } from './apiSrv/interface/index.interfaces'
-export { magnamentStorage } from './magnamentStorage'
+export { magnamentStorage } from './magnamentStorage/index'
